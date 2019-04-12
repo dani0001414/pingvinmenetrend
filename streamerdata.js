80b667ed2b5db792f6723a28dfde65f231204039
dani0001414 <dani0001414@gmail.com>
2019-04-12 16:55:30 +0200

    NewSite

---
+++ streamerdata.js
@@ -0,0 +1,14 @@
+/**MobilBarát Menetrend Testreszabása. */
+/*Streamer adatok megadása*/
+
+//A twitch neved amivel a csatorna elérhető --> http://twitch.tv/twitchneved.
+var streamer = "fyrexxx";
+//Kép megjelenítése amikor üres a menetrend.
+var noEventsPic = "pingvin.png";
+//Kép megjelenítése amikor offline a menetrend(nembiztos, hogy megjelenik majd offline állapotban)
+var offlinePic = "pingvin.png";
+//Offline állapotban megjelenő szöveg
+var offlineText = "Nincs internet!"; 
+//Üres menetrendkor megjelenő szöveg
+var noEventsText = "Menetrend Jelenleg üres!";
+
